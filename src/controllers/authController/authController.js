0d62435fb0b300f60d7c8e5cc341d9e7444dad3f
saulm96 <saulmorahernandez96@gmail.com>
2025-03-09 00:00:39 +0100

    Fixed a few things related with the refactorize of the User model in sequelize. Also added a basic local register system which still needs some work

--- src/controllers/authController/authController.js
+++ src/controllers/authController/authController.js
@@ -1,5 +1,4 @@
 import { UserMethods } from "../../models/user/userMethods.js";
-import {sign} from "../../config/jwt.js";
 import error from "../../utils/errors/userErrors.js";
 
 
@@ -11,14 +10,31 @@ async function regularLogin(userData) {
     }
 
     const user = await UserMethods.authenticateLocalUser(email, password);
-    if(!user) throw new error.INVALID_CREDENTIALS_IN_LOGIN();
+    if (!user) throw new error.INVALID_CREDENTIALS_IN_LOGIN();
 
     return user;
 }
 
+async function regularRegister(userData) {
+    const { firstName, lastName, username, email, password, confirmedPassword } = userData;
+
+    if (!firstName || !lastName || !username || !email || !password || !confirmedPassword) {
+        throw new error.MISSING_PARAMETERS();
+    }
+    
+
+    if (password !== confirmedPassword) {
+        throw new error.PASSWORDS_DONT_MATCH();
+    }
+
+    delete userData.condirmedPassword;
+
+    return await UserMethods.createUserUsingRegularRegister(userData);
+}
 
 export const functions = {
-    regularLogin
+    regularLogin,
+    regularRegister
 }
 
 export default functions;
